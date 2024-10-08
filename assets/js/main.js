2993921cca3c357a8279033b594bc5d09f5feb90
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-10-08 15:39:38 -0300

    fix smooth scrool

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,8 +19,9 @@
 
     //Smoth Scroll
     $(function () {
-        $('.nav-link, .smoth-scroll').on('click', function (event) {
+        $('.nav-link,.smooth-scroll').on('click', function (event) {
             var $anchor = $(this);
+            
             $('html, body').stop().animate({
                 scrollTop: $($anchor.attr('href')).offset().top - 0
             }, 1000);
